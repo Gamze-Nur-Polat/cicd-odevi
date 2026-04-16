ddf32440c4b0053d74a397570a6cc8f2b9b73f25
Gamze-Nur-Polat <gamzenurpolat384@gmail.com>
2026-04-16 10:53:46 +0300

    sunum güncelleme

--- src/main/java/com/example/cicd_odevi/TestController.java
+++ src/main/java/com/example/cicd_odevi/TestController.java
@@ -14,7 +14,7 @@ public class TestController {
         String zaman = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
 
         return "<html><body style='text-align:center; font-family:sans-serif; background-color:#f4f4f4;'>" +
-                "<h1 style='color:#2c3e50;'>🚀 CI/CD Otomasyonu Yayında!</h1>" +
+                "<h1 style='color:#2c3e50;'>🚀---- CI/CD Otomasyonu Yayında!</h1>" +
                 "<p style='font-size:1.2em;'>Merhaba Hocam, sistem başarıyla çalışıyor.</p>" +
                 "<div style='background:white; display:inline-block; padding:20px; border-radius:10px; shadow: 2px 2px 5px #ccc;'>" +
                 "<strong>Geliştirici:</strong> Gamze Nur Polat <br>" +
